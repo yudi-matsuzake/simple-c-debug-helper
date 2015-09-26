e539fff81ff404fee6acda7a6d219e8c9e348cc6
Yudi Matsuzake <yudi.matsuzake@gmail.com>
2015-09-26 12:09:30 -0300

    Fabulous debugs! (colors added)

--- debug.hpp
+++ debug.hpp
@@ -14,7 +14,40 @@ using namespace std;
 
 /*	MACROS	*/
 /*
-MACRO FUNCTION TO_STRING (X)
+COLORS
+=============================
+Implements some MACROS colors
+and MACROS flags for fabulous
+colorized DEBUG messages */
+
+//colors
+#define _NONE	"\033[0m"
+#define _GRAY	"\033[38;5;247m"
+#define _RED	"\033[1;31m"
+#define _GREEN	"\033[1;32m"
+#define _YELLOW	"\033[1;33m"
+#define _BLUE	"\033[38;5;26m"
+
+//functions
+#ifdef DEBUG_COLOR_ENABLE
+	#define GRAY(X)		_GRAY << X << _NONE
+	#define RED(X)		_RED << X << _NONE
+	#define GREEN(X)	_GREEN << X << _NONE
+	#define YELLOW(X)	_YELLOW << X << _NONE
+	#define BLUE(X)		_BLUE << X << _NONE
+#else
+	#define GRAY(X) X 
+	#define RED(X) X
+	#define GREEN(X) X
+	#define YELLOW(X) X
+	#define BLUE(X) X
+#endif
+
+/*STRINGIFY*/
+#define STR(X) #X
+
+
+/* MACRO FUNCTION TO_STRING (X)
 =============================
 To do something like that:
 
@@ -45,11 +78,36 @@ This MACRO calls the debug() with the macro __PRETTY_FUNCTION__ that returns the
 /*	FUNCTIONS 	*/
 
 void debug(string f, string msg, unsigned int n){
-	#ifdef DEBUG_SHOW_PREFIX
-		cout << "DEBUG:(" << n << ")";
+	//prefix
+	#ifdef DEBUG_PREFIX_ENABLE
+		cout << RED("DEBUG")":";
 	#endif
+	
+	//depth
+	cout << "(" << YELLOW(n) << ")";
+
+	//indent
 	for(int i=0; i<(n*2); i++) cout << ' ';
-	cout << f << ": " << msg << endl;
+
+	//function name and message
+	cout << BLUE(f) << ": " << GRAY(msg) << endl;
 }
+
+//undef colors
+#undef NONE	
+#undef GRAY	
+#undef RED
+#undef GREEN	
+#undef YELLOW	
+#undef BLUE	
+#undef _NONE	
+#undef _GRAY	
+#undef _RED
+#undef _GREEN	
+#undef _YELLOW	
+#undef _BLUE	
+
+//undef strigify
+#undef STR
 
 #endif //_DEBUG_H_
